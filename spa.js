af8e7d2ee2a049190b59dcabc7d8ea1f541f29df
inumagic <inumagic@gmail.com>
2025-04-10 14:30:41 +0800

    Delete spa.js

--- spa.js
+++
@@ -1,7 +0,0 @@
-  document.addEventListener('DOMContentLoaded', () => {
-    const carouselElement = document.querySelector('#medicalCarousel');
-    const carousel = new bootstrap.Carousel(carouselElement, {
-      interval: 5000, // 5 秒自動切換
-      pause: 'hover'
-    });
-  });
