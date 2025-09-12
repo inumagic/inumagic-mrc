49ca7970548c9e20ed13bc7240d407b8befb9dfa
inumagic <inumagic@gmail.com>
2025-09-12 17:13:22 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -72,26 +72,35 @@
 
 
     // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1000,
-        center: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            }
+    // Testimonials carousel
+$(".testimonial-carousel").owlCarousel({
+    autoplay: false,
+    smartSpeed: 1000,
+    center: true,
+    dots: false,
+    loop: true,
+    nav : true,
+    
+    responsive: {
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3   // 桌機顯示三個
         }
-    });
+    }
+});
+
+$('#owl-testimonial').owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+});
 
     
 })(jQuery);
